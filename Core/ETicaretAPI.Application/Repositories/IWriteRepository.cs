ab40d479c2e1bcebdbd1b852ca27c2319b476c94
emre3 <emre3@LAPTOP-L3IAVLVM>
2024-02-26 22:57:31 +0300

    26 Şubat 2024

---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteRepository<T>:IRepsitory<T> where T:class
+    {
+       Task<bool>AddAsync(T model);
+        Task<bool> AddAsync(List<T>model);
+        Task<bool> RemoveAsync(T model);
+        Task<bool> RemoveAsync(string id);
+        Task<bool> UpdateAsync(T model);
+    }
+}
